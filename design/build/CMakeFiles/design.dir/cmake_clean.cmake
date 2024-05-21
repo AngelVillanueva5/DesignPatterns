5acd388de73cfd0b45de245605943718f6b23ce3
AngelVillanueva5 <sd.villanueva@student.avans.nl>
2024-05-21 09:58:50 +0200

    setup for project

---
+++ design/build/CMakeFiles/design.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/design.dir/src/main.cpp.o"
+  "CMakeFiles/design.dir/src/main.cpp.o.d"
+  "design"
+  "design.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/design.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
